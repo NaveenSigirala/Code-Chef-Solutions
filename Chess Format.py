9393bc84d586f876c3d799f73142964752f9d55d
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-02-16 19:41:21 +0530

    Create Chess Format.py

---
+++ Chess Format.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    if (a+b) < 3:
+        print("1")
+    elif 3 <= (a+b) <= 10:
+        print("2")
+    elif 11 <= (a+b) <= 60:
+        print("3")
+    else:
+        print("4")
