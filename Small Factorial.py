09cdc69df4a6340d28a97ccb98b786dbc011da46
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-02-17 20:32:56 +0530

    Create Small Factorial.py

---
+++ Small Factorial.py
@@ -0,0 +1,10 @@
+t=int(input())
+for i in range(t):
+    def factrorial(n):
+        if n==0:
+            return 1
+        else:
+            result= n*factrorial(n-1)
+            return result
+    l=int(input())
+    print(factrorial(l))
