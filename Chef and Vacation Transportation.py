263dbf84f620abbfd7f9727c8b0a0ae21dcb9082
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-02-20 19:21:43 +0530

    Create Chef and Vacation Transportation.py

---
+++ Chef and Vacation Transportation.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    x,y,z=map(int,input().split())
+    if (x + y) > z:
+        print("TRAIN")
+    elif (x + y) < z:
+        print("PLANEBUS")
+    else:
+        print("EQUAL")
