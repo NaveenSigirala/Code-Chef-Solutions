c8d3bbae8d2dc76e68ba866c8e7b53b660450661
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-02-23 20:37:23 +0530

    Create Problem Category.py

---
+++ Problem Category.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    x=int(input())
+    if x>=1 and x<100:
+        print("easy")
+    elif x>=100 and x<200:
+        print("medium")
+    else:
+        print("hard")
