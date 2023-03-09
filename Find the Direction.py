701ac9b20dfe0bf8167ed45be54b94cc7e63f160
NaveenSigirala <101165307+NaveenSigirala@users.noreply.github.com>
2023-03-09 20:54:26 +0530

    Create Find the Direction.py

---
+++ Find the Direction.py
@@ -0,0 +1,11 @@
+for i in range(int(input())):
+    x=int(input())
+    if x%4==0:
+        print("north")
+    elif x%4==1:
+        print("east")
+    elif x%4==2:
+        print("south")
+    elif x%4==3:
+        print("west")
+    continue
